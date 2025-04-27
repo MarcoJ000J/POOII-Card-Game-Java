d9a574870a2daccd0b419322ac03721d412f5fbc
Marco Antônio <Marco Antnio@DESKTOP-0AO94NL>
2025-04-27 19:44:56 -0300

    Starting

---
+++ CardGameXD/src/cardForNow/Card.java
@@ -0,0 +1,5 @@
+package cardForNow;
+
+public class Card {
+
+}
---
+++ CardGameXD/src/main/GamePanel.java
@@ -0,0 +1,17 @@
+package main;
+
+import javax.swing.JFrame;
+
+public class GamePanel extends JFrame {
+	
+	
+	public GamePanel() {
+		setTitle("A INCRIVEL XD Card Game");
+		setSize(1000,600);
+		
+		setLocationRelativeTo(null);
+		
+		setResizable(false);
+		setVisible(true);
+	}
+}
