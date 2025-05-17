7a4cddbdcccc28012222fe1cedddf85b29350dc8
Mahavii <120056895+LokSchrod@users.noreply.github.com>
2025-05-17 23:59:36 +0000

    Classe menu ajeitada esperando mais att e estudo

--- CardGameXD/src/menu/Menu.java
+++ CardGameXD/src/menu/Menu.java
@@ -1,6 +1,43 @@
 package main;
 
-public class Menu {
+import javax.swing.*;
+import java.awt.*;
+import dialogo.ControladorDialogo;
+import cardForNow.*;
+import util.CarregadorImagem;
+
+public class Menu extends JFrame {
+	public Menu () {
+
+		super("Orv GAME");
+		setDefaultCloseOperation(EXIT_ON_CLOSE);
+		setSize(1280, 720);
+		setLocationRelativeTo(null);
+		setLayout(null);
+
+		//fundo 
+		JLabel fundo = new JLabel (CarregadorImagem.load("menu_background"));
+		fundo.setBounds(0, 0, 1280, 720);
+		setContentPane(fundo);
+		fundo.setLayout(null);
+		//fundo.setLayout(new BorderLayout());
+		//fundo.setLayout(new GridLayout(1, 1));
+		//fundo.setLayout(new FlowLayout());
+		//fundo.setLayout(new BoxLayout(fundo, BoxLayout.Y_AXIS));
+		//fundo.setLayout(new GridBagLayout());
+
+		//botao Funcionais
+
+		int margenX = 50, margemY = getHeight() - 220;
+		String[] nomes = {"Novo Jogo", "Continuar", "Configs" , "Sair"};
+		Jbutton [] botoes = new JButton (textos[i]);
+		for (int i=0; i<4; i++)
+
+
+
+
+	}
+
 	/* 
 	 * fazer aqui o menu pricipal, onde de começa o jogo novo (futuramente continuar um jogo antigo)
 	 * sair do jogo e ver placar de lideres e onde vai se escolher a dificuldade 
